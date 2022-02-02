80372f434048f669ffb756915af7067f00cd8901
Yug Jain <yugjain1995@gmail.com>
2022-02-01 20:57:06 -0500

    Restructured for code reusability

--- src/camera_motion_extraction/test_image_subscriber.cpp
+++ src/camera_motion_extraction/test_image_subscriber.cpp
@@ -1,10 +1,21 @@
-
 /// [headers]
 #include<image_subscriber.h>
+#include<string>
 /// [headers]
 
 int main(int argc, char **argv){
-    RosToCvmat img_conv;
-    img_conv.imageSubscriber(true, argc, argv);
-    return 0;
+  bool disp = false; // Display or Not the image recived from camera
+
+// Parse options from argument list
+  for(int i = 0; i<argc; i++){
+    if(strcmp(argv[i], "-d") == 0){
+      disp = true;
+      break;
+    }
+  }
+
+// Test image subscription
+  RosToCvmat img_conv;
+  img_conv.imageSubscriber(disp, argc, argv);
+  return 0;
 }
