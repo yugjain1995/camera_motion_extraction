732143fabedff40300eb58dd1fad21ff11030f2a
yugjain1995 <yugjain1995@gmail.com>
2022-07-04 12:37:50 -0400

    Re-positioned code blocks and added implementation for cameraPoseEstimate() function.

--- src/camera_motion_extraction/motion_extraction.cpp
+++ src/camera_motion_extraction/motion_extraction.cpp
@@ -11,10 +11,37 @@ a monocular camera.
 /// [headers]
 #include <ros/console.h>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/calib3d/calib3d.hpp"
 #include <motion_extraction.h>
 #include <chrono>
 /// [headers]
 
+
+/******************************************************************/
+MotionEstimate2D2D::imageCompute(){
+  this->matchAndDisplay();
+  this->cameraPoseEstimate();
+}
+/******************************************************************/
+
+/******************************************************************/
+MotionEstimate2D2D::cameraPoseEstimate(){
+// Start the timer
+  auto start = std::chrono::high_resolution_clock::now();
+// Get essential matrix from keypoints
+  findEssentialMat();
+// Recover pose from essential matrix
+  recoverPose();
+// End the timer and compute time elapsed
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+// Display Pose
+  ROS_INFO_STREAM( "\nR = \n" << _R << "\n" );
+  ROS_INFO_STREAM( "\nt = \n" << _t << "\n" );
+// Display time elapsed
+  ROS_INFO_STREAM( "Total time to estimate pose = " << std::to_string(duration.count()) << std::endl );
+}
+/******************************************************************/
 
 /******************************************************************/
 MotionEstimate2D2D::MotionEstimate2D2D(){
@@ -27,7 +54,6 @@ MotionEstimate2D2D::MotionEstimate2D2D(){
 }
 /******************************************************************/
 
-// 2 weekday
 /******************************************************************/
 MotionEstimate2D2D::findEssentialMat(){
   
@@ -38,20 +64,5 @@ MotionEstimate2D2D::findEssentialMat(){
 /******************************************************************/
 MotionEstimate2D2D::recoverPose(){
   
-}
-/******************************************************************/
-
-// 2 weekday
-/******************************************************************/
-MotionEstimate2D2D::cameraPoseEstimate(){
-  
-}
-/******************************************************************/
-
-
-/******************************************************************/
-MotionEstimate2D2D::imageCompute(){
-  this->matchAndDisplay();
-  this->cameraPoseEstimate();
 }
 /******************************************************************/
